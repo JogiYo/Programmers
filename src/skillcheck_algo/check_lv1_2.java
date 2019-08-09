afeafe8cb9500a3638e6b5d6f50f9abb7e5ffdb4
JogiYo <jo930921@gmail.com>
2019-08-09 15:58:53 +0900

    Programmers, check_lv1_2 Problems of arrangement d with the amount requested by department  and how many departments can support the goods when given as parameters.

---
+++ src/skillcheck_algo/check_lv1_2.java
@@ -0,0 +1,40 @@
+package skillcheck_algo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// Programmers, check_lv1_2
+// Problems of arrangement d with the amount requested by department 
+// and how many departments can support the goods when given as parameters.
+public class check_lv1_2 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int budget = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] d = new int[st.countTokens()];
+		for(int i = 0; i < d.length; i++) {
+			d[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		sb.append(solution(d, budget) + "");
+		System.out.print(sb);
+	}
+	
+	public static int solution(int[] d, int budget) {
+	      int answer = 0;
+	      
+	      Arrays.sort(d);
+
+	      for(int i = 0; i < d.length-1; i++) {
+	    	  budget -= d[i];
+	    	  if(budget < 0) break;
+	    	  answer++;
+	      }
+	      return answer;
+	  }
+}

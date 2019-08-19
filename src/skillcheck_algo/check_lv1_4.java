70ce6b7f5ef2ce5f6aa604abc7685f91e1d72413
JogiYo <jo930921@gmail.com>
2019-08-19 17:15:35 +0900

    Recovered accidentally deleted files.

---
+++ src/skillcheck_algo/check_lv1_4.java
@@ -0,0 +1,33 @@
+package skillcheck_algo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class check_lv1_4 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String[] input = br.readLine().split(" ");
+		int x = Integer.parseInt(input[0]);
+		int n = Integer.parseInt(input[1]);
+		
+		long[] answer = solution(x,n);
+		sb.append("[");
+		for(int i = 0; i < answer.length-1; i++) {
+			sb.append(answer[i] +",");
+		}
+		sb.append(answer[answer.length-1] + "]");
+		System.out.print(sb);
+	}
+	
+	public static long[] solution(int x, int n) {
+		long[] answer = new long[n];
+		long interval = x;
+		for(int i = 0; i < n; i++) {
+			answer[i] = interval;
+			interval += x;
+		}
+		return answer;
+	}
+}

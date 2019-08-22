bb06c88ef8c7414a51aaa00b829cd73659c48edf
JogiYo <jo930921@gmail.com>
2019-08-22 15:07:32 +0900

    Prorammers, Level 1 : short coding(1-7) using Arrays.stream

--- src/skillcheck_algo_lv1/Leve1_7_short.java
+++ src/skillcheck_algo_lv1/Leve1_7_short.java
@@ -2,12 +2,9 @@ package skillcheck_algo_lv1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-
-// Programmers, Level 1 : Create Divided Numeric Arrangement
-// it would be beneficial to study 'Arrays.stream'.
-public class Level1_7 {
+// Prorammers, Level 1 : short coding(1-7) using Arrays.stream
+public class Leve1_7_short {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -20,33 +17,24 @@ public class Level1_7 {
 		}
 		int[] answer = solution(input, divisor);
 		
-		sb.append("[" + answer[0] + ", ");
-		for(int i = 1; i < answer.length-1; i++) {
-			sb.append(answer[i] + ", ");
+		if(answer.length == 1) sb.append("[" + answer[0] + "]");
+		else {
+			sb.append("[" + answer[0] + ", ");
+			for(int i = 1; i < answer.length-1; i++) {
+				sb.append(answer[i] + ", ");
+			}
+			sb.append(answer[answer.length-1] + "]");
 		}
-		sb.append(answer[answer.length-1] + "]");
 		System.out.print(sb);
 	}
 	
 	public static int[] solution(int[] arr, int divisor) {
-		int[] answer;
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i]%divisor == 0) list.add(arr[i]);
-		}
-		
-		if(list.isEmpty()) {
+		int[] answer = Arrays.stream(arr).filter(factor -> factor % divisor == 0).toArray();
+		if(answer.length == 0) {
 			answer = new int[1];
 			answer[0] = -1;
 		}
-		else {
-			answer = new int[list.size()];
-			for(int i = 0; i < answer.length; i++) {
-				answer[i] = list.get(i);
-			}
-			Arrays.sort(answer);
-		}
+		else Arrays.sort(answer);
 		return answer;
 	}
 }

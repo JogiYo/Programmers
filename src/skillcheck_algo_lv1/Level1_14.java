29ceaea133ce5b5df2b47377863b404ee8d1d4b2
JogiYo <jo930921@gmail.com>
2019-08-23 15:58:06 +0900

    Eratosthenes optimize

--- src/skillcheck_algo_lv1/Level1_14.java
+++ src/skillcheck_algo_lv1/Level1_14.java
@@ -3,7 +3,8 @@ package skillcheck_algo_lv1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-// Programmers Level 1 : Find prime number
+// Programmers Level 1 : Find prime number 
+// Using Eratosthenes Algorithm!
 public class Level1_14 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,19 +17,25 @@ public class Level1_14 {
 	
 	public static int solution(int n) {
 		int answer = 0;
+		int[] number = new int[n+1];
+	
+		for(int i = 2; i <= n; i++)	number[i] = i;
 		
-		if(n == 2) return 1;
-		for(int i = 2; i <= n; i++) {
-			boolean isPrime = true;
-			
-			for(int j = 2; j < i; j++) {
-				if(i%j == 0) {
-					isPrime = false;
-					break;
+		// 2 ~ i*i <= n
+		// Clear each multiple.
+		for(int i = 2; (i*i) <= n; i++) {
+			if(number[i] != 0) {
+				for(int j = i*2; j <= n; j += i) {
+					number[j] = 0;
+					// Since i * i has already been processed, 
+					// the starting value of j can be optimized with i * i.
+					// for(int j = i*i; j <= n; j += i)
 				}
 			}
-			
-			if(isPrime) answer++;
+		}
+		// If the array value is not zero, it's prime number.
+		for(int i = 0; i < number.length; i++) {
+			if(number[i] != 0) answer++;
 		}
 		return answer;
 	}

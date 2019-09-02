b50f427deb816606eb102b18652648393a093ea4
JogiYo <jo930921@gmail.com>
2019-09-02 17:14:23 +0900

    Programmers Level 1 : Pointing Rectangular star

--- src/skillcheck_algo_lv1/Level1_30.java
+++ src/skillcheck_algo_lv1/Level1_30.java
@@ -1,5 +1,29 @@
 package skillcheck_algo_lv1;
 
-public class Level1_30 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+// Programmers Level 1 : Pointing Rectangular star
+public class Level1_30 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String[] input = br.readLine().split(" ");
+		int a = Integer.parseInt(input[0]);
+		int b = Integer.parseInt(input[1]);
+		sb.append(solution(a,b) + "");
+		System.out.print(sb);
+	}
+	
+	public static String solution(int a, int b) {
+		String answer = "";
+		
+		for(int i = 0; i < b; i++) {
+			for(int j = 0; j < a; j++) {
+				answer += "*";
+			}
+			answer +="\n";
+		}
+		return answer;
+	}
 }

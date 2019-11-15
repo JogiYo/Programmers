6b6a6ff6d17fe124aaf6db62762aa578d6604328
JogiYo <jo930921@gmail.com>
2019-11-15 17:24:38 +0900

    Programmers Level 2 : A fine rectangle -ing (2)

--- src/skillcheck_algo_lv2/Level2_12.java
+++ src/skillcheck_algo_lv2/Level2_12.java
@@ -1,5 +1,51 @@
 package skillcheck_algo_lv2;
 
-public class Level2_12 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+//Programmers Level 2 : A fine rectangle
+public class Level2_12 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+	
+		String[] input = br.readLine().split(", ");
+		int w = Integer.parseInt(input[0]);
+		int h = Integer.parseInt(input[1]);
+		
+		sb.append(solution(w,h) + "");
+		
+		System.out.print(sb);
+	}
+	
+	public static long solution(int w,int h) {
+		long answer = w*h;
+        long cd = gcd(w,h);
+        
+        long sw = w/cd;
+        long sh = h/cd;
+        
+        answer -= cd * sw * sh;
+        
+        long safe = 0;
+        
+        for(int i = 1; i < sw; i++){
+            safe += i * sh/sw;
+        }
+        
+        answer += safe * 2 * cd;
+        return answer;
+    }
+	
+    public static long gcd(int a, int b){
+        while(a!=0){
+            if(a<b){
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            a = a-b;
+        }
+        return b;
+    }
 }

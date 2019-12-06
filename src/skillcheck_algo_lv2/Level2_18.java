3694061f16dce9ab7f3f49e206ef6148a40f5b23
JogiYo <jo930921@gmail.com>
2019-12-06 14:34:05 +0900

    Programmers Level 2 : Find Prime number Using Permutation, Exclude Duplicates(Using HashSet)

--- src/skillcheck_algo_lv2/Level2_18.java
+++ src/skillcheck_algo_lv2/Level2_18.java
@@ -23,12 +23,20 @@ public class Level2_18 {
         	perm(list, 0, list.length, i, primeList);
         }
         
+        answer = primeList.size();
+        		
         return answer;
     }
 
 	private static void perm(char[] list, int depth, int n, int r, Set<Integer> primeList) {
 		if(depth == r) {
-			print(list, r, primeList);
+			StringBuilder s = new StringBuilder();
+			
+			for(int i = 0; i < r; i++) {
+				s.append(list[i]);
+			}
+			
+			isPrime(primeList, s);
 			return;
 		}
 		
@@ -40,16 +48,27 @@ public class Level2_18 {
 		
 	}
 
-	private static void print(char[] list, int r, Set<Integer> primeList) {
-		StringBuilder s = new StringBuilder();
-		for(int i = 0; i < r; i++) {
-			
-		}
-	}
-
 	private static void swap(char[] list, int depth, int i) {
 		char temp = list[depth];
 		list[depth] = list[i];
 		list[i] = temp;
+	}
+	
+	private static void isPrime(Set<Integer> primeList, StringBuilder s) {
+		int x = Integer.parseInt(String.valueOf(s));
+		boolean prime = true;
+		
+		if( x <= 1) {
+			return;
+		}
+		for(int i = 2; i <= Math.sqrt(x); i++) {
+			if(x % i == 0) {
+				prime = false;
+				break;
+			}
+		}
+		if(prime) {
+			primeList.add(x);
+		}
 	}
 }

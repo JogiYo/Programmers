d63c4a4c5b27bcffd03acf07d78654979997014e
JogiYo <jo930921@gmail.com>
2020-01-31 16:18:56 +0900

    Programmers Level 2 : Candidate Key (2019 KAKAO BLIND RECRUITMENT) Using Bit Mask

--- src/skillcheck_algo_lv2/Level2_47.java
+++ src/skillcheck_algo_lv2/Level2_47.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// Programmers Level 2 : Candidate Key
+// Programmers Level 2 : Candidate Key (2019 KAKAO BLIND RECRUITMENT)
 // Using Bit Mask
 public class Level2_47 {
 	public static void main(String[] args) throws Exception{

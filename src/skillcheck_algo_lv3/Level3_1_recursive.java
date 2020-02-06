9659349a2cef4d53fa674e84cbb37227777dc2c0
JogiYo <jo930921@gmail.com>
2020-02-06 17:53:33 +0900

    Programmers Level 3 : Origami (Using recursion)

--- src/skillcheck_algo_lv3/Level3_1_recursive.java
+++ src/skillcheck_algo_lv3/Level3_1_recursive.java
@@ -1,5 +1,33 @@
 package skillcheck_algo_lv3;
 
-public class Level3_1_recursive {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Level3_1_recursive {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+	
+		int n = Integer.parseInt(br.readLine());
+		int[] ans = solution(n);
+		
+		for(int i = 0; i < ans.length; i++) {
+			sb.append(ans[i] + " ");
+		}
+		
+		System.out.print(sb);
+	}
+	
+	public static int[] solution(int n) {
+		if(n == 1) return new int[] {0};
+		int[] temp = solution(n-1);
+		int[] answer = new int[temp.length*2 + 1];
+		
+		for(int i = 0; i < answer.length; i++) {
+			if(i < temp.length) answer[i] = temp[i];
+			else if(i == temp.length) answer[i] = 0;
+			else answer[i] = temp[answer.length-1-i] == 1 ? 0 : 1;
+		}
+		return answer;
+    }
 }

e3a0bcb84e90d5cb21ec2aaa26a0bfc7f4003659
JogiYo <jo930921@gmail.com>
2020-02-10 16:27:45 +0900

    Programmers Level 3 : Thanks giving day traffic important to convert time into seconds

--- src/skillcheck_algo_lv3/Level3_2.java
+++ src/skillcheck_algo_lv3/Level3_2.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 // Programmers Level 3 : Thanks giving day traffic
+// important to convert time into seconds
 public class Level3_2 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,41 +24,47 @@ public class Level3_2 {
 	public static int solution(String[] lines) {
 		int answer = 0;
 		int traffic = 0;
-		double S, T, section_start, section_end;
 		
+		float[] start_time = new float[lines.length];
+		float[] end_time = new float[lines.length];
+		
+		// convert time into seconds
 		for(int i = 0; i < lines.length; i++) {
 			String[] str = lines[i].split(" ");
 			String[] time_str = str[1].split(":");
-			double time_hour = Double.parseDouble(time_str[0]);
-			double time_minute = Double.parseDouble(time_str[1]);
-			double time_second = Double.parseDouble(time_str[2]);
-			
-			S = (int)time_hour*3600 + (int)time_minute*60 + time_second;
-			T = Double.parseDouble(str[2].replace("s", ""));
-			
-			section_start = S - T + 0.001;
-			section_end = section_start + 0.999;
+			int time_hour = Integer.parseInt(time_str[0]);
+			int time_minute = Integer.parseInt(time_str[1]);
+			float time_second = Float.parseFloat(time_str[2]);
+			// End_time = T (Response Time)
+			end_time[i] = time_hour*3600 + time_minute*60 + time_second;
+			float T = Float.parseFloat(str[2].replace("s", ""));
+			start_time[i] = end_time[i] - T + 0.001f;
+		}
 		
-			for(int j = i; j < lines.length; j++) {
-				str = lines[j].split(" ");
-				time_str = str[1].split(":");
-				time_hour = Double.parseDouble(time_str[0]);
-				time_minute = Double.parseDouble(time_str[1]);
-				time_second = Double.parseDouble(time_str[2]);
-				
-				S = (int)time_hour*3600 + (int)time_minute*60 + time_second;
-				T = Double.parseDouble(str[2].replace("s", "")) - 0.001;
-				double start_time = S - T + 0.001;
-				double end_time = S;
-				
-				if(section_start <= start_time || end_time <= section_end) traffic++;
-				else break;
+		for(int i = 0; i < lines.length; i++) {
+			// Based on start time
+			for(int j = 0; j < lines.length; j++) {
+				if((start_time[i] >= start_time[j] && start_time[i] <= end_time[j])
+						|| (start_time[i] + 0.999f >= start_time[j] && start_time[i] + 0.999f <= end_time[j])
+						|| (start_time[i] <= start_time[j] && start_time[i] + 0.999f >= end_time[j])) {
+					traffic++;
+				}
 			}
 			
-			if(answer < traffic) {
-				answer = traffic;
-				traffic = 0;
+			if(traffic > answer) answer = traffic;
+			traffic = 0;
+			
+			//// Based on end time
+			for(int j = 0; j < lines.length; j++) {
+				if((end_time[i] >= start_time[j] && end_time[i] <= end_time[j])
+						|| (end_time[i] + 0.999f >= start_time[j] && end_time[i] + 0.999f <= end_time[j])
+						|| (end_time[i] + 0.999f >= start_time[j] && end_time[i] <= end_time[j])) {
+					traffic++;
+				}
 			}
+			
+			if(traffic > answer) answer = traffic;
+			traffic = 0;
 		}
 		
 		return answer;

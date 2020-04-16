c2e5da0fdb177635892558172ccfe635bd41de7b
JogiYo <jo930921@gmail.com>
2020-04-16 16:19:17 +0900

    한 단어에 같은 규칙이 두 번 이상 적용될 수 없을을 캐치해야함

--- src/skillcheck_algo_lv3/Level3_5.java
+++ src/skillcheck_algo_lv3/Level3_5.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
-// Programmers Level 3 : Brian's troubles
+// Programmers Level 3 : Brian's troubles (2017 KAKAO CODE Qualifier)
 public class Level3_5 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

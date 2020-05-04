250c830dac12929afc781c664713e630d52fd05d
JogiYo <jo930921@gmail.com>
2020-05-04 16:59:57 +0900

    계속 수정중

--- src/skillcheck_algo_lv3/Level3_5.java
+++ src/skillcheck_algo_lv3/Level3_5.java
@@ -7,7 +7,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 // Programmers Level 3 : Brian's troubles (2017 KAKAO CODE Qualifier)
-// test case : https://programmers.co.kr/learn/questions/9618
+// test case : https://programmers.co.kr/learn/questions/9618 
+// test case 2 : https://github.com/wh2per/Programmers-Algorithm/blob/master/Programmers/Lv3/Lv3_%EB%B8%8C%EB%9D%BC%EC%9D%B4%EC%96%B8%EC%9D%98%EA%B3%A0%EB%AF%BC.cpp
 public class Level3_5 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,6 +37,7 @@ public class Level3_5 {
 			}
 		}
 		
+		boolean rule2_used = false;
 		for(int i = 0; i < sentence.length(); i++) {
 			char ch = sentence.charAt(i);
 			if(ch == ' ') return "invalid";
@@ -55,6 +57,7 @@ public class Level3_5 {
 					sentence = sentence.substring(i+2);
 					word = word.replaceAll(Character.toString(ch), "");
 					qu.offer(word);
+					if(rule2_used) rule2_used = false;
 				}
 				// rule 1
 				// ex) ABAaAaAaA
@@ -64,6 +67,7 @@ public class Level3_5 {
 					int start = i - 1;
 					int end = sentence.lastIndexOf(ch) + 1;
 					
+					if(end == sentence.length()) return "invalid"; 
 					for(int j = i; j <= sentence.lastIndexOf(ch); j += 2) {
 						if(sentence.charAt(j) != ch) return "invalid";
 						if(i != 0 && !(sentence.charAt(i-1) >= 65 && sentence.charAt(i-1) <= 90)) return "invalid";
@@ -86,6 +90,7 @@ public class Level3_5 {
 						}
 					}
 					qu.offer(word);
+					if(rule2_used) rule2_used = false;
 				}
 				
 				// rule 1 and rule 2
@@ -111,14 +116,46 @@ public class Level3_5 {
 					int rule_count = 0;
 					String word;
 					if(i != 0 && upper_case_cnt == 1) {
-						word = sentence.substring(start-1, end+2);
-						if(start-1 != 0) {
-							qu.offer(sentence.substring(0, start-1));
+						if(rule2_used) {
+							word = sentence.substring(start-1, end+2);
+							if(start-1 != 0) {
+								qu.offer(sentence.substring(0, start-1));
+							}
+							char cha = word.charAt(i);
+							word = word.replaceAll(Character.toString(cha), "");
+							sentence = sentence.substring(end+2);
 						}
-						char cha = word.charAt(i);
-						word = word.replaceAll(Character.toString(cha), "");
-						sentence = sentence.substring(end+2);
+						else {
+							word = sentence.substring(start+1, end);
+							if(start != 0) {
+								qu.offer(sentence.substring(0, start));
+							}
+							sentence = sentence.substring(end+1);
+						}
+						//if(end == sentence.length()-1) {
+						//	word = sentence.substring(start+1, end);
+						//	if(start != 0) {
+						//		qu.offer(sentence.substring(0, start));
+						//	}
+						//	sentence = sentence.substring(end+1);
+						//}
+						//else {
+						//	word = sentence.substring(start+1, end);
+						//	if(start != 0) {
+						//		qu.offer(sentence.substring(0, start));
+						//	}
+						//	sentence = sentence.substring(end+1);
+							// AaAaAcA
+							//word = sentence.substring(start-1, end+2);
+							//if(start-1 != 0) {
+							//	qu.offer(sentence.substring(0, start-1));
+							//}
+							//char cha = word.charAt(i);
+							//word = word.replaceAll(Character.toString(cha), "");
+							//sentence = sentence.substring(end+2);
+						//}
 						qu.offer(word);
+						if(rule2_used) rule2_used = false;
 					}
 					else {
 						if(start != 0) {
@@ -130,7 +167,8 @@ public class Level3_5 {
 							char cha = word.charAt(t);
 							if(set.containsKey(cha)) {
 								rule_use = true;
-								if(set.get(cha) == 2 && word.length() != 5) return "invalid";
+								if(word.length() != set.get(cha)*2 + 1) return "invalid";
+								//if(set.get(cha) == 2 && word.length() != 5) return "invalid";
 								else {
 									word = word.replaceAll(Character.toString(cha), "");
 									rule_count++;
@@ -142,6 +180,7 @@ public class Level3_5 {
 						
 						if(rule_use) {
 							sentence = sentence.substring(start+1, end) + sentence.substring(end + 1);
+							rule2_used = true;
 						}
 						else {
 							sentence = sentence.substring(end+1);

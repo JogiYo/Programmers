dea24faeff773246972420b2246f26ecbfa1f217
JogiYo <jo930921@gmail.com>
2020-09-09 16:38:48 +0900

    Programmers Level 3 : Immigration using binary search

--- src/skillcheck_algo_lv3/Level3_18.java
+++ src/skillcheck_algo_lv3/Level3_18.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 // Programmers Level 3 : Immigration
-// 
+// using binary search
 public class Level3_18 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,17 +25,28 @@ public class Level3_18 {
 	
 	public static long solution(int n, int[] times) {
         long answer = 0;
+        // Total time required for immigration
+        // left : minimum, right : maximum 
         long left = 0, mid = 0, sum = 0;
         Arrays.sort(times);
-        long right = times.length * times[times.length-1];
+        // The times array is of type int, so it needs casting
+        long right = (long)n * times[times.length-1];
         answer = right;
         
+        // Finding out if the sum of the number of jobs processed 
+        // at each immigration desk is N
         while(left <= right) {
         	sum = 0;
         	mid = (left + right) / 2;
         	for(int i : times) {
         		sum += mid / i;
         	}
+        	
+        	if(sum >= n) {
+        		if(mid <= answer) answer = mid;
+        		right = mid - 1;
+        	}
+        	else left = mid + 1;
         }
         
         return answer;

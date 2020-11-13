a44d26f0c5781deb6d003c6a78ed5972a3115a0a
JogiYo <jo930921@gmail.com>
2020-11-13 16:58:58 +0900

    Programmers Level 3 : How to line up Find an efficient way

--- src/skillcheck_algo_lv3/Level3_32.java
+++ src/skillcheck_algo_lv3/Level3_32.java
@@ -2,7 +2,10 @@ package skillcheck_algo_lv3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+// Programmers Level 3 : How to line up
+// Find an efficient way
 public class Level3_32 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,41 +23,26 @@ public class Level3_32 {
 		}
 		System.out.print(sb);
 	}
-	
-	static int count = 0;
+	// Using permutation causes a timeout.
+	// Find a way to find a number in a specific place.
 	public static int[] solution(int n, long k) {
         int[] answer = new int[n];
-        int[] arr = new int[n];
-        int[] output = new int[n];
-        boolean[] visit = new boolean[n];
-        
-        for(int i = 0; i < n; i++) {
-        	arr[i] = i+1;
+        ArrayList<Integer> list = new ArrayList<>();
+        long facto = 1;
+        for(int i = 1; i <= n; i++) {
+        	list.add(i);
+        	facto *= i;
         }
+        k--; // k starts from 0
         
-        perm(arr, output, visit, 0, n, n, answer, k);
+        int idx = 0;
+        while(n > 0) {
+        	facto /= n;  // ex) (1, 2, 3), find 5th. facto = 6/3 = 2  
+        	answer[idx++] = list.get((int)(k/facto)); // k/facto = 4/2 = 2 -> 3
+        	list.remove((int)(k/facto));
+        	k %= facto;
+        	n--;
+        }
         return answer;
     }
-
-	private static void perm(int[] arr, int[] output, boolean[] visit, int depth, int n, int r, int[] answer, long k) {
-		if(depth == r) {
-			count++;
-			if((int)k == count) {
-				for(int i = 0; i < r; i++) {
-					answer[i] = output[i];
-				}
-			}
-		}
-		
-		for(int i = 0; i < n; i++) {
-			if(!visit[i]) {
-				visit[i] = true;
-				output[depth] = arr[i];
-				perm(arr, output, visit, depth+1, n, r, answer, k);
-				visit[i] = false;
-				if(count == (int) k) break;
-			}
-		}
-		
-	}
 }

115e3c0ba45a90f264fe10fd51edd7f14ba60bbf
JogiYo <jo930921@gmail.com>
2021-05-06 16:17:59 +0900

    Programmers Level 2 : Rotate matrix border(2021 Dev-Matching: Web Backend Developer (First Half)) implementation

--- src/skillcheck_algo_lv2/Level2_61.java
+++ src/skillcheck_algo_lv2/Level2_61.java
@@ -3,6 +3,8 @@ package skillcheck_algo_lv2;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+// Programmers Level 2 : Rotate matrix border(2021 Dev-Matching: Web Backend Developer (First Half))
+// implementation
 public class Level2_61 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,6 +23,12 @@ public class Level2_61 {
 		}
 		
 		int[] result = solution(rows, columns, query);
+		sb.append(result[0] + "");
+		
+		for(int i = 1; i < result.length; ++i) {
+			sb.append(", " + result[i]);
+		}
+		
 		System.out.print(sb);
 	}
 	
@@ -28,11 +36,11 @@ public class Level2_61 {
 	public static int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
         matrix = new int[rows][columns];
-        int num = 1;
         
+        int num = 1;       
         for(int i = 0; i < rows; ++i) {
         	for(int j = 0; j < columns; ++j) {
-        		matrix[i][j] = i++;
+        		matrix[i][j] = num++;
         	}
         }
         
@@ -43,8 +51,32 @@ public class Level2_61 {
         return answer;
     }
 	
-	private static int rotate(int[] is) {
-		// TODO Auto-generated method stub
-		return 0;
+	private static int rotate(int[] qr) {
+		int r1 = qr[0] - 1;
+		int c1 = qr[1] - 1;
+		int r2 = qr[2] - 1;
+		int c2 = qr[3] - 1;
+		
+		int temp = matrix[r1][c1];
+		int min = temp;
+		for(int i = r1; i < r2; ++i) {
+			matrix[i][c1] = matrix[i+1][c1];
+			min = Math.min(min, matrix[i][c1]);
+		}
+		for(int i = c1; i < c2; ++i) {
+			matrix[r2][i] = matrix[r2][i+1];
+			min = Math.min(min, matrix[r2][i]);
+		}
+		for(int i = r2; i > r1; --i) {
+			matrix[i][c2] = matrix[i-1][c2];
+			min = Math.min(min, matrix[i][c2]);
+		}
+		for(int i = c2; i > c1; --i) {
+			matrix[r1][i] = matrix[r1][i-1];
+			min = Math.min(min, matrix[r1][i]);
+		}
+		matrix[r1][c1+1] = temp;
+		
+		return min;
 	}
 }

9874400711cddb2dd093bfb2c42960807f9c0e03
JogiYo <jo930921@gmail.com>
2022-04-19 16:33:14 +0900

    -ing...

--- src/skillcheck_algo_lv2/Level2_72.java
+++ src/skillcheck_algo_lv2/Level2_72.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Level2_72 {
-    static int[] res = {-1};
+    static int[] result = {-1};
     static int[] ryan;
     static int max = Integer.MIN_VALUE;
     public static void main(String[] args) throws Exception{
@@ -19,10 +19,10 @@ public class Level2_72 {
             info[i] = Integer.parseInt(input[i]);
         }
 
-        res = solution(n, info);
+        result = solution(n, info);
 
-        for(int i = 0; i < res.length; ++i) {
-            sb.append(res[i] + " ");
+        for(int i = 0; i < result.length; ++i) {
+            sb.append(result[i] + " ");
         }
         System.out.print(sb);
     }
@@ -30,10 +30,31 @@ public class Level2_72 {
     public static int[] solution(int n, int[] info) {
         ryan = new int[11];
         dfs(info, 1, n);
-        return res;
+        return result;
     }
 
     private static void dfs(int[] info, int cnt, int n) {
+        if(cnt == n+1) {
+            int apeachp = 0;
+            int ryanp = 0;
 
+            for(int i = 0; i < 11; ++i) {
+                if(info[i] != 0 || ryan[i] != 0) {
+                    if(info[i] < ryan[i]) ryanp += 10 - i;
+                    else apeachp += 10 - i;
+                }
+            }
+
+            if(ryanp > apeachp) {
+                if(ryanp - apeachp > max) {
+                    result = ryan.clone();
+                    max = ryanp - apeachp;
+                }
+            }
+
+            return;
+        }
+
+        for(int i = 0; i < 11 && ry)
     }
 }

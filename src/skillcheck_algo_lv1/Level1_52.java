26557e65c2277a4e5582d0a6e07e361f073438e1
JogiYo <jo930921@gmail.com>
2022-10-28 15:57:01 +0900

    Level 1, 52 -ing

---
+++ src/skillcheck_algo_lv1/Level1_52.java
@@ -0,0 +1,49 @@
+package skillcheck_algo_lv1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Level1_52 {
+    static String[] pronun = {"aya", "ye", "woo", "ma"};
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] babbling = br.readLine().split(" ");
+
+        sb.append(solution(babbling));
+        System.out.print(sb);
+    }
+
+    public static int solution(String[] babbling) {
+        int answer = 0;
+
+        for (String s : babbling) {
+            String temp = s;
+            int length = temp.length();
+            int cur = -1;
+
+            for (int j = 0; j < 4; ++j) {
+                if (temp.contains(pronun[j]) && cur != j) {
+                    if (j == 0 || j == 2) {
+                        temp = temp.replaceFirst(pronun[j], "000");
+                    } else {
+                        temp = temp.replaceFirst(pronun[j], "00");
+                    }
+
+                    cur = j;
+                    j = -1;
+                }
+            }
+
+            int count = 0;
+            for (int k = 0; k < temp.length(); ++k) {
+                if (temp.charAt(k) == '0') count++;
+            }
+
+            if (count == length) answer++;
+        }
+        return answer;
+    }
+}

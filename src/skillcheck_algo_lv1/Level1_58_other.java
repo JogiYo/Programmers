45e833c665aab05fa95cfc16949edb47e2e939ab
JogiYo <jo930921@gmail.com>
2022-12-05 16:34:09 +0900

    Programmers Level 1 : Split string more effective solution

--- src/skillcheck_algo_lv1/Level1_58_other.java
+++ src/skillcheck_algo_lv1/Level1_58_other.java
@@ -3,7 +3,9 @@ package skillcheck_algo_lv1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Level1_58_ohter {
+// Programmers Level 1 : Split string
+// more effective solution
+public class Level1_58_other {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -15,6 +17,28 @@ public class Level1_58_ohter {
     }
 
     private static int solution(String s) {
-        
+        char prev = 'A';
+        int x_count = 0, other_count = 0, answer = 0;
+
+        for(char c : s.toCharArray()) {
+            // when the first letter
+            if(prev == 'A') {
+                prev = c;
+                x_count++;
+                answer++;
+            }
+            // When it is a letter like x
+            else if(prev == c) x_count++;
+            else other_count++;
+
+            // Split strings when number of characters are equal
+            if(x_count == other_count) {
+                prev = 'A';
+                x_count = 0;
+                other_count = 0;
+            }
+        }
+
+        return answer;
     }
 }

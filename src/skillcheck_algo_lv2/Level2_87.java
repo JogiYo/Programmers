00d8189a531a6f9915e676885954eb5f5d1cd9fb
JogiYo <jo930921@gmail.com>
2023-01-09 16:18:16 +0900

    Programmers Level 2 : Pseudo Cantor bitstream Check whether there is a zero or not by going up from the bottom line

--- src/skillcheck_algo_lv2/Level2_87.java
+++ src/skillcheck_algo_lv2/Level2_87.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// Programmers Level 2 : Pseudo Cantor bitstream
 public class Level2_87 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,6 +27,29 @@ public class Level2_87 {
          * n=2, 11011 11011 00000 11011 11011
          */
         int answer = 0;
+
+        for(long i = l; i <= r; ++i) {
+            int flag = 1;
+            if(i % 5 == 3) continue;
+
+            long temp = i;
+
+            // Check whether there is a zero or not by going up from the bottom line
+            while(true) {
+                if(temp % 5 == 0) temp /= 5;
+                else temp = (temp / 5) + 1;
+
+                if(temp %5 == 3) {
+                    flag = 0;
+                    break;
+                }
+                else if(temp <= 5) {
+                    break;
+                }
+            }
+
+            if(flag == 1) answer++;
+        }
 
         return answer;
     }

fc0d17eb0ceca56c18d7a4c6ffc0930a292ac69b
JogiYo <jo930921@gmail.com>
2023-01-31 16:15:02 +0900

    Programmers Level 2 : Converting numbers use HashSet or BFS (bfs is recommended)

--- src/skillcheck_algo_lv2/Level2_89.java
+++ src/skillcheck_algo_lv2/Level2_89.java
@@ -3,11 +3,14 @@ package skillcheck_algo_lv2;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 // Programmers Level 2 : Converting numbers
-// use HashSet
 public class Level2_89 {
+    private static int[] visitCount = new int[10000001];
+    private static Queue<Integer> queue = new LinkedList<>();
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -23,6 +26,7 @@ public class Level2_89 {
         System.out.print(sb);
     }
 
+    // use HashSet
     public static int solution(int x, int y, int n) {
         int cnt = 0;
         HashSet<Integer> curSet = new HashSet<>(), nextSet = null;
@@ -46,5 +50,28 @@ public class Level2_89 {
         }
 
         return -1;
+    }
+
+    // use bfs
+    public static int solution_bfs(int x, int y, int n) {
+        queue.offer(x);
+
+        while(!queue.isEmpty()) {
+            int value = queue.poll();
+            if(value == y) return visitCount[value];
+
+            addQueue(y, value, value + n);
+            addQueue(y, value, value * 2);
+            addQueue(y, value, value * 3);
+        }
+
+        return -1;
+    }
+
+    private static void addQueue(int y, int value, int next) {
+        if(next <= y && visitCount[next] == 0) {
+            queue.offer(next);
+            visitCount[next] = visitCount[value] + 1;
+        }
     }
 }

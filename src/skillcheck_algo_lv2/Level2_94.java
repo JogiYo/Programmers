d0778eb262e6b6331e43b9247e9e4aa9547cbf4a
JogiYo <jo930921@gmail.com>
2023-02-21 16:44:36 +0900

    Programmers Level 2 : Escape the maze use BFS

--- src/skillcheck_algo_lv2/Level2_94.java
+++ src/skillcheck_algo_lv2/Level2_94.java
@@ -4,9 +4,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
+// Programmers Level 2 : Escape the maze
+// use BFS
 public class Level2_94 {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -23,59 +26,53 @@ public class Level2_94 {
     }
 
     private static int solution(String[] maps) {
-        int[][] maze = new int[maps.length][maps[0].length()];
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        int sx = 0, sy = 0;
-        int ex = -1, ey = -1;
+        // S to L -> L to S
+        int StoL = bfs(maps, 'S', 'L');
+        int LtoE = bfs(maps, 'L', 'E');
 
-        // 틀린 이유 : 무조건 레버를 내려야만 탈출을 할 수 있다. 그러므로 시작점에서 레버가 있는 곳으로 간 후에
-        // 다시 레버에서 출구로 가는 가장 짧은 구간을 구해야 한다.
+        if(StoL == -1 || LtoE == -1) return -1;
+        return StoL + LtoE;
+    }
+
+    private static int bfs(String[] maps, char start, char end) {
+        boolean[][] maze = new boolean[maps.length][maps[0].length()];
+        int sx = -1, sy = -1;
+
         for(int i = 0; i < maze.length; ++i) {
             for(int j = 0; j < maze[0].length; ++j) {
-                if(maps[i].charAt(j) == 'X') maze[i][j] = 0;
-                else if(maps[i].charAt(j) == 'S') {
-                    maze[i][j] = 0;
+                if(maps[i].charAt(j) == start) {
                     sx = i;
                     sy = j;
-                }
-                else if(maps[i].charAt(j) == 'O' || maps[i].charAt(j) == 'L') maze[i][j] = 1;
-                else if(maps[i].charAt(j) == 'E') {
-                    maze[i][j] = -1;
-                    ex = i;
-                    ey = j;
+                    break;
                 }
             }
+            if(sx != -1) break;
         }
 
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{sx, sy});
+        queue.offer(new int[]{sx, sy, 0});
+        maze[sx][sy] = true;
 
         while(!queue.isEmpty()) {
             int[] point = queue.poll();
             int x = point[0];
             int y = point[1];
-            boolean flag = false;
+            int depth = point[2];
+
+            if(maps[x].charAt(y) == end) return depth;
 
             for(int i = 0; i < 4; ++i) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
                 if(nx < 0 || ny < 0 || nx >= maze.length || ny >= maze[0].length) continue;
-                if(maze[nx][ny] == 0) continue;
-                if(maze[nx][ny] == 1) {
-                    maze[nx][ny] = maze[x][y] + 1;
-                    queue.offer(new int[]{nx, ny});
-                }
-                if(maze[nx][ny] == -1) {
-                    maze[nx][ny] = maze[x][y] + 1;
-                    flag = true;
+                if(maps[nx].charAt(ny) != 'X' && !maze[nx][ny]) {
+                    queue.offer(new int[]{nx, ny, depth+1});
+                    maze[nx][ny] = true;
                 }
             }
-
-            if(flag) break;
         }
 
-        return maze[ex][ey];
+        return -1;
     }
 }

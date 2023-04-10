2b89dd330ddbe80994187531dae75506bba8beea
JogiYo <jo930921@gmail.com>
2023-04-10 16:04:58 +0900

    Programmers Level 2 : Sum of consecutive subsequences use Accumulated sum & Two Pointer

--- src/skillcheck_algo_lv2/Level2_98.java
+++ src/skillcheck_algo_lv2/Level2_98.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// Programmers Level 2 : Sum of consecutive subsequences
+// use Accumulated sum & Two Pointer
 public class Level2_98 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,19 +27,30 @@ public class Level2_98 {
         int[] answer = new int[2];
         int maxLen = Integer.MAX_VALUE;
 
+        // use Accumulated sum
         int[] accumulatedSum = new int[sequence.length+1];
         for(int i = 0; i < sequence.length; ++i) accumulatedSum[i+1] = accumulatedSum[i] + sequence[i];
 
+        // start two pointer
+        /*
+         * If the current sum is less than K, right is raised by +1.
+         * If not, increment left by +1.
+         */
         int left = 0, right = 0;
         while(left <= right) {
             if(left >= accumulatedSum.length || right >= accumulatedSum.length) break;
             int sum = accumulatedSum[right] - accumulatedSum[left];
 
+            /*
+             * When an answer sequence is found,
+             * the length of the sequence is checked and converted into the sequence with the smallest length.
+             */
             if(sum == k) {
                 int curLen = right - left;
                 if(maxLen > curLen) {
                     answer[0] = left;
                     answer[1] = right-1;
+                    maxLen = curLen;
                 }
             }
 

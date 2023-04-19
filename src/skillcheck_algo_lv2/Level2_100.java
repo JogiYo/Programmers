554490dbb457de34c589e7be4f7da22c6a717d83
JogiYo <jo930921@gmail.com>
2023-04-19 16:26:39 +0900

    Programmers Level 2 : Integer pairs between two circles Using the properties of a circle (x^2 + y^2 = r^2)

--- src/skillcheck_algo_lv2/Level2_100.java
+++ src/skillcheck_algo_lv2/Level2_100.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// Programmers Level 2 : Integer pairs between two circles
+// Using the properties of a circle (x^2 + y^2 = r^2)
 public class Level2_100 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,9 +21,23 @@ public class Level2_100 {
     }
 
     private static long solution(int r1, int r2) {
-        long r1Count = (long)(4 + Math.pow(2*r1-1, 2));
-        long r2Count = (long)(4 + Math.pow(2*r2-1, 2));
+        long answer = 0;
 
-        return r2Count - r1Count + 4;
+        long r1pow = (long)Math.pow(r1, 2);
+        long r2pow = (long)Math.pow(r2, 2);
+
+        // y^2 = r^2 - x^2
+        for(int i = 1; i <= r2; ++i) {
+            /*
+             * Since it is a pair of integers,
+             * floor function and ceil function are used.
+             */
+            long y2 = (long)Math.floor(Math.sqrt(r2pow - (long)Math.pow(i,2)));
+            long y1 = (long)Math.ceil(Math.sqrt(r1pow - (long)Math.pow(i,2)));
+
+            answer += (y2 - y1 + 1);
+        }
+
+        return answer * 4;
     }
 }
